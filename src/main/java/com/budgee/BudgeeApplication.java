50799e80bfd535c3b692a58b2c7f559b4752ea44
vandunxg <nguyenvandung.official@gmail.com>
2025-10-04 23:17:57 +0700

    init budgee

---
+++ src/main/java/com/budgee/BudgeeApplication.java
@@ -0,0 +1,13 @@
+package com.budgee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/budgee/BudgeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
