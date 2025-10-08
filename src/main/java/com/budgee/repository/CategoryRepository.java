8ca8e223e56b9fdf744169456e1eb1adc21ef2ff
vandunxg <nguyenvandung.official@gmail.com>
2025-10-08 14:54:30 +0700

    add @Repository annotation

--- src/main/java/com/budgee/repository/CategoryRepository.java
+++ src/main/java/com/budgee/repository/CategoryRepository.java
@@ -5,5 +5,7 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.budgee.model.Category;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {}
