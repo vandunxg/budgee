c0afa77fbfc8a713742fb77d7b9298473507adad
vandunxg <nguyenvandung.official@gmail.com>
2025-10-09 22:10:57 +0700

    update function name

--- src/main/java/com/budgee/controller/client/CategoryController.java
+++ src/main/java/com/budgee/controller/client/CategoryController.java
@@ -28,47 +28,47 @@ public class CategoryController {
 
     CategoryService categoryService;
 
-    @GetMapping("/list")
-    ResponseEntity<?> getAllCategoryWithSortBy(
-            @RequestParam(defaultValue = "0", required = false) int pageNo,
-            @Min(10) @RequestParam(defaultValue = "10", required = false) int pageSize,
-            @RequestParam(required = false) String sortBy) {
-        log.info("[GET /list/pageNo={}&pageSize={}&sortBy={}]", pageNo, pageSize, sortBy);
-
-        return ResponseUtil.success(
-                MessageConstants.FETCH_SUCCESS,
-                categoryService.getAllCategoriesWithSortBy(pageNo, pageSize, sortBy));
-    }
-
     @RequestMapping("/{id}")
-    ResponseEntity<?> getCategory(@PathVariable UUID id) {
-        log.info("[GET /category/{}]", id);
+    ResponseEntity<?> get(@PathVariable UUID id) {
+        log.info("[GET /categories/{}]", id);
 
         return ResponseUtil.success(MessageConstants.FETCH_SUCCESS, categoryService.getCategory(id));
     }
 
     @PostMapping("/")
-    ResponseEntity<?> createCategory(@Valid @RequestBody CategoryRequest request) {
-        log.info("[POST /category/] {}", request.toString());
+    ResponseEntity<?> create(@Valid @RequestBody CategoryRequest request) {
+        log.info("[POST /categories/] {}", request.toString());
 
         return ResponseUtil.created(categoryService.createCategory(request));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<?> updateCategory(
+    public ResponseEntity<?> update(
             @PathVariable UUID id, @RequestBody CategoryUpdateRequest request) {
-        log.info("[PATCH /category/{}]={}", id, request.toString());
+        log.info("[PATCH /categories/{}]={}", id, request.toString());
 
         return ResponseUtil.success(
                 "Updated successfully", categoryService.updateCategory(request, id));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> updateCategory(@PathVariable UUID id) {
-        log.info("[DELETE /category/{}]", id);
+    public ResponseEntity<?> delete(@PathVariable UUID id) {
+        log.info("[DELETE /categories/{}]", id);
 
         categoryService.deleteCategory(id);
 
         return ResponseUtil.deleted();
+    }
+
+    @GetMapping("/list")
+    ResponseEntity<?> list(
+            @RequestParam(defaultValue = "0", required = false) int pageNo,
+            @Min(10) @RequestParam(defaultValue = "10", required = false) int pageSize,
+            @RequestParam(required = false) String sortBy) {
+        log.info("[GET /categories/list/pageNo={}&pageSize={}&sortBy={}]", pageNo, pageSize, sortBy);
+
+        return ResponseUtil.success(
+                MessageConstants.FETCH_SUCCESS,
+                categoryService.getAllCategoriesWithSortBy(pageNo, pageSize, sortBy));
     }
 }
