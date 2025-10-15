6be2e5824dc14ba96db2db6ca36dffef8ba60d66
vandunxg <nguyenvandung.official@gmail.com>
2025-10-15 14:53:58 +0700

    refactor transaction service

--- src/main/java/com/budgee/service/TransactionService.java
+++ src/main/java/com/budgee/service/TransactionService.java
@@ -1,7 +1,10 @@
 package com.budgee.service;
 
+import java.util.List;
 import java.util.UUID;
 
+import com.budgee.model.Category;
+import com.budgee.model.Transaction;
 import com.budgee.payload.request.TransactionRequest;
 import com.budgee.payload.response.TransactionResponse;
 
@@ -12,6 +15,8 @@ public interface TransactionService {
     TransactionResponse updateTransaction(UUID id, TransactionRequest request);
 
     TransactionResponse getTransaction(UUID id);
+
+    List<Transaction> getTransactionByCategory(Category category);
 
     void deleteTransaction(UUID id);
 }
