5fc174bda1878b7ce6a7402970dd0c57e3adfb26
vandunxg <nguyenvandung.official@gmail.com>
2025-10-22 21:46:24 +0700

    fix cors config

--- src/main/java/com/budgee/config/WebSecurityConfig.java
+++ src/main/java/com/budgee/config/WebSecurityConfig.java
@@ -92,15 +92,14 @@ public class WebSecurityConfig {
     // config cors
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("**")
-                        .allowedOrigins("http://localhost:8500")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
-                        .allowedHeaders("*") // Allowed request headers
-                        .allowCredentials(false)
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
                         .maxAge(3600);
             }
         };
