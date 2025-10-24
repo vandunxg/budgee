f16f4a9d84bddbbf94f580276dd49736bbddf764
vandunxg <nguyenvandung.official@gmail.com>
2025-10-24 22:26:57 +0700

    refactor: enhance `TransactionMapper` and streamline `TransactionServiceImpl`
    - Updated `TransactionMapper` to support additional parameters (`wallet`, `category`, `user`) and ignore `type` during mapping.
    - Simplified `TransactionServiceImpl` by leveraging updated mapper, removing redundant method calls and unused repository.
    - Introduced `setTransactionType` private method for cleaner type setting logic.

--- src/main/java/com/budgee/mapper/TransactionMapper.java
+++ src/main/java/com/budgee/mapper/TransactionMapper.java
@@ -1,5 +1,8 @@
 package com.budgee.mapper;
 
+import com.budgee.model.Category;
+import com.budgee.model.User;
+import com.budgee.model.Wallet;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -10,7 +13,11 @@ import com.budgee.payload.response.TransactionResponse;
 @Mapper(componentModel = "spring")
 public interface TransactionMapper {
 
-    Transaction toTransaction(TransactionRequest request);
+    @Mapping(target = "user", source = "user")
+    @Mapping(target = "wallet", source = "wallet")
+    @Mapping(target = "category", source = "category")
+    @Mapping(target = "type", ignore = true)
+    Transaction toTransaction(TransactionRequest request, Wallet wallet, Category category, User user);
 
     @Mapping(target = "walletId", source = "wallet.id")
     @Mapping(target = "transactionId", source = "id")
