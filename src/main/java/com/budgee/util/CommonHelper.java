1553b8a228b17ebad29e98b5cbafda539a44b899
vandunxg <nguyenvandung.official@gmail.com>
2025-10-25 00:09:12 +0700

    refactor: replace `Helpers` with `CommonHelper` for better utility focus
    - Removed `Helpers` class as part of utility consolidation.
    - Introduced `updateIfChanged` method in `CommonHelper` to simplify property updates.
    - Streamlined helper methods by adopting a focused and reusable utility design.

---
+++ src/main/java/com/budgee/util/CommonHelper.java
@@ -0,0 +1,47 @@
+package com.budgee.util;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j(topic = "HELPERS")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class CommonHelper {
+
+    // -------------------------------------------------------------------
+    // REPOSITORY
+    // -------------------------------------------------------------------
+
+    // -------------------------------------------------------------------
+    // SERVICE
+    // -------------------------------------------------------------------
+
+    // -------------------------------------------------------------------
+    // MAPPER
+    // -------------------------------------------------------------------
+
+    // -------------------------------------------------------------------
+    // HELPER
+    // -------------------------------------------------------------------
+
+    public <T> void updateIfChanged(Supplier<T> getter, Consumer<T> setter, T newValue) {
+        log.info("[updateIfChanged]");
+
+        T oldValue = getter.get();
+
+        if (!Objects.equals(oldValue, newValue)) {
+            log.trace("[updateIfChanged] Updated field from {} to {}", oldValue, newValue);
+
+            setter.accept(newValue);
+        }
+    }
+}
