60f80defee8edfb92e16ba3e35d118c787c2e4c2
vandunxg <nguyenvandung.official@gmail.com>
2025-10-29 22:31:22 +0700

    feat(goal): add lookup services and simplify validation logic
    - Introduced `WalletLookup` and `CategoryLookup` services for streamlined entity retrieval and ownership validation.
    - Replaced redundant methods and dependencies in `GoalServiceImpl` with centralized lookups (e.g., `getGoalForCurrentUser`, `getWalletForCurrentUser`).
    - Implemented `GoalValidator` to consolidate update and validation logic, enhancing maintainability.
    - Removed unnecessary `validateDates` method and simplified date validation with `DateValidator`.
    - Refactored dependency usage in `GoalServiceImpl` for better modularity and reduced code duplication.

---
+++ src/main/java/com/budgee/service/lookup/WalletLookup.java
@@ -0,0 +1,50 @@
+package com.budgee.service.lookup;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+import com.budgee.exception.ErrorCode;
+import com.budgee.exception.NotFoundException;
+import com.budgee.model.Wallet;
+import com.budgee.repository.WalletRepository;
+import com.budgee.util.SecurityHelper;
+
+@Component
+@Slf4j(topic = "WALLET-LOOKUP")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class WalletLookup {
+
+    // -------------------------------------------------------------------
+    // REPOSITORY
+    // -------------------------------------------------------------------
+    WalletRepository walletRepository;
+
+    // -------------------------------------------------------------------
+    // UTILITIES
+    // -------------------------------------------------------------------
+    SecurityHelper securityHelper;
+
+    public Wallet getWalletById(UUID id) {
+        log.info("[getWalletById]={}", id);
+
+        return walletRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.WALLET_NOT_FOUND));
+    }
+
+    public Wallet getWalletForCurrentUser(UUID walletId) {
+        log.info("[getWalletForCurrentUser] categoryId={}", walletId);
+
+        Wallet wallet = this.getWalletById(walletId);
+        securityHelper.checkIsOwner(wallet);
+
+        return wallet;
+    }
+}
