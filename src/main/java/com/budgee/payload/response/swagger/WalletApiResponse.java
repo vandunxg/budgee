b80a6428ebd0d445c543cfd722bf6ff3d66b6001
vandunxg <nguyenvandung.official@gmail.com>
2025-11-01 17:05:10 +0700

    refactor(wallet): optimize imports and improve Swagger annotations
    - Reorganized imports in `WalletController` and `WalletApiResponse` for clarity and consistency.
    - Tweaked Swagger annotations in the ` getWallet ` method to enhance readability and maintain consistent formatting.

--- src/main/java/com/budgee/payload/response/swagger/WalletApiResponse.java
+++ src/main/java/com/budgee/payload/response/swagger/WalletApiResponse.java
@@ -1,9 +1,9 @@
 package com.budgee.payload.response.swagger;
 
-import com.budgee.payload.response.ApiResponse;
-import com.budgee.payload.response.RegisterResponse;
-import com.budgee.payload.response.WalletResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
+
+import com.budgee.payload.response.ApiResponse;
+import com.budgee.payload.response.WalletResponse;
 
 @Schema(description = "API Response containing created user")
 public class WalletApiResponse extends ApiResponse<WalletResponse> {}
